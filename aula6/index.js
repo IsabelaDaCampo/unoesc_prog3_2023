dd54fcf0d962d190430600dbacb47a08a88b91db
Isabela Maria <isabeladacampo@gmail.com>
2023-04-11 21:25:01 -0300

    faltou arquivos

--- aula6/index.js
+++ aula6/index.js
@@ -5,7 +5,9 @@ const port = 4000;
 const { 
   getProducts,
   getProduct,
-  createProduct
+  createProduct,
+  updateProduct,
+  deleteProduct
 } = require('./controllers/productController');
 
 const server = http.createServer((req, res) => {
@@ -15,11 +17,32 @@ const server = http.createServer((req, res) => {
     getProducts(req, res);
   } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'GET'){
     const id = req.url.split('/')[3];
-    
     getProduct(req, res, id);
   } else if(req.url === '/api/products' && req.method === 'POST'){
     createProduct(req, res);
-  } else {
+  } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'PUT') {
+    const id = req.url.split('/')[3];
+    updateProduct(req, res, id);
+  } else if(req.url.match(/\/api\/products\/\w+/) && req.method === 'DELETE') {
+    const id = req.url.split('/')[3];
+    deleteProduct(req, res, id);
+  } else if(req.url === '/products' && req.method === 'GET'){
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.end(`
+    <html>
+      <head>
+        <title>Produtos</title>
+      </head>
+      <body>
+        <h1>Produtos</h1>
+        <ul>
+          ${product.map(prod => `<li>${prod.name}</li>`).join('')}
+        </ul>
+      </body>
+    </html>
+  `);
+  } 
+  else {
     res.end(JSON.stringify({
       message: 'Route not found. Please use the api/products route.'
     }));
